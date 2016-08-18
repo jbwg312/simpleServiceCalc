acec22f0a8bc61a579826be781f4d379737923db
jbwg312 <jbwg312@gmail.com>
2016-08-18 10:57:06 -0600

    Finished thing

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,13 @@
-trig.controller("TrigController", function(){
-
-  
+trig.controller("TrigController", function($scope, CalculatorFactory){
+	$scope.sideA;
+	$scope.sideB;
+	$scope.shape;
+	$scope.half;
+	$scope.calculate = function(){
+		$scope.hypotenuse = CalculatorFactory.hyp($scope.sideA, $scope.sideB)
+	};
+	$scope.calculate2 = function(){
+		$scope.area = CalculatorFactory.radi($scope.half)
+	};
 
 });
--- app/js/services.js
+++ app/js/services.js
@@ -1,7 +1,14 @@
 trig.factory("CalculatorFactory", function(){
-
-
-
-
-
+	return {
+		hyp: function(sideA, sideB){
+			var start = (sideA * sideA) + (sideB * sideB);
+			return (Math.sqrt(start)).toFixed(2)
+		},
+		radi: function(radi){
+			return (2 * Math.PI * radi).toFixed(2)
+		},
+		rom: function(sideAA, sideBB){
+			return 
+		}
+	}
 })
